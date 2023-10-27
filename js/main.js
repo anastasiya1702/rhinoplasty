adae2134eac05e2723eddf7b7579a92b5024274c
anastasiya1702 <voronovanastya988@gmail.com>
2023-10-27 13:08:04 +0300

    added section difference

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
-  
+  $('.slider').slick({
+    dots: true,
+    arrows: true,
+    fade: true,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 1600,
+    infinite: false,
+  });
 
 });
