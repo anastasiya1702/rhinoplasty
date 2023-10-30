034a49850d2309b9feb1c09a48ccfc65578f2b33
anastasiya1702 <voronovanastya988@gmail.com>
2023-10-30 19:19:49 +0200

    added section details

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
+  $('.details-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.details-tabs__top-item').removeClass('details-tabs__top-item--active');
+    $(this).addClass('details-tabs__top-item--active');
+
+    $('.details-tabs__content-item').removeClass('details-tabs__content-item--active');
+    $($(this).attr('href')).addClass('details-tabs__content-item--active');
+  });
+
   $('.slider').slick({
     dots: true,
     arrows: true,
