53ed4bf6ac56547d6adbee57d87a9a90e2a84e35
anastasiya1702 <voronovanastya988@gmail.com>
2023-11-08 10:58:08 +0200

    added section review

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 $(function () {
 
+  // $('.product-slide__thumb').slick({
+  //   asNavFor: '.product-slide__big',
+  //   focusOnSelect: true,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   vertical: true,
+  //   draggable: false
+  // });
+
+  // $('.product-slide__big').slick({
+  //   asNavFor: '.product-slide__thumb',
+  //   draggable: false,
+  //   arrows: false,
+  //   fade: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1051,
+  //       settings: {
+  //         draggable: true,
+  //       }
+  //     },
+  //   ]
+  // });
+
+  $('.review-slider').slick({
+    dots: true,
+    arrows: true,
+    fade: true,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 1600,
+    infinite: false,
+  });
+
   $('.details-tabs__top-item').on('click', function (e) {
     e.preventDefault();
     $('.details-tabs__top-item').removeClass('details-tabs__top-item--active');
